8336d24d120efa70f8aa2cb3f9892b5cdd7fec9d
Onur Hangül <onrhng@gmail.com>
2019-11-19 22:20:53 +0100

    All component is ready

--- src/App.js
+++ src/App.js
@@ -1,28 +1,69 @@
-import React from 'react';
+import React,  {Component} from 'react';
 import Particles from 'react-particles-js';
+import Clarifai from'clarifai';
 import './App.css';
-import Navigation from './components/navigation/Navigation';
-import Logo from './components/logo/Logo';
-import ImageLink from './components/imagelink/ImageLink';
-import Info from './components/info/Info';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLink from './components/ImageLink/ImageLink';
+import Info from './components/Info/Info';
+import Image from './components/Image/Image';
 
-class App extends React.Component {
-  render (){
+const app = new Clarifai.App({
+ apiKey: '6b3aeb6efa814671beaaf12c6aa52c11'
+});
+
+class App extends Component {
+ constructor (){
+    super()
+    this.state = {
+      input:'',
+      imageUrl:'',
+      box:{}
+    }
+  }
+
+  callFaceLocation = (data) => {
+    const faceData = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const face = document.getElementById('inputimage');
+    const width = Number(face.width);
+    const height = Number(face.height);
+    return {
+      leftCol:faceData.left_col * width,
+      topRow: faceData.top_row *height,
+      rightCol: width - (faceData.right_col * width),
+      bottomRow: height - (faceData.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) =>{
+    console.log(box);
+    this.setState ({box: box});
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input});
+    app.models.predict(
+      Clarifai.FACE_DETECT_MODEL, 
+      this.state.input)
+    .then(response => this.displayFaceBox(this.callFaceLocation(response)))
+    .catch(err => console.log(err));
+  }
+
+  render() {
     return (
       <div>
         <Particles className='particles' />
         <Navigation />
         <Logo />
         <Info />
-        <ImageLink />
-          
-     
-        
-        {/*
-                
-               
-                <Image />
-        */}
+        <ImageLink 
+          onInputChange={this.onInputChange} 
+          onButtonSubmit={this.onButtonSubmit}/>
+        <Image box={this.state.box} imageUrl={this.state.imageUrl}/>
        </div>
     )
   }  
--- src/components/imagelink/ImageLink.js
+++ src/components/imagelink/ImageLink.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import './ImageLink.css';
 
-const ImageLink = () => {
-	return(
-	  <div>
-		<p className='f3 center'>
-			{'This Magic App will detect faces in your photos'}
-		</p>
-		<div className='center'>
-		  <div className='patterns center pa4 br3 shadow-5 '>
-			<input className='f4 pa2 br1 w-80 center' type='text'/>
-			<button className='w-20 grow br2 f4 link ph3 pv2 dib white bg-light-purple'>Start</button>
-		  </div>
-		</div>
-	  </div>	
+const ImageLink = ({onInputChange, onButtonSubmit}) => {
+ return(
+   <div>
+	<p className='f3 center'>
+	  This Magic App will detect faces in your photos. Try it...
+	</p>
+	<div className='center'>
+	  <div className='patterns center pa4 br3 shadow-5 '>
+		<input  
+			className='f4 pa2 br1 w-80 center' 
+			type='text' 
+			onChange={onInputChange}/>
+		<button 
+			onClick={onButtonSubmit}
+			className='w-20 grow br2 f4 link ph3 pv2 dib white bg-light-purple'>Start</button>
+	  </div>
+	</div>
+  </div>	
 	)
 }
 export default ImageLink; 
