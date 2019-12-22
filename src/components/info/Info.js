cb9396486e2cd6bdcea3b12b74c323f0d7d49855
Onur Hangül <onrhng@gmail.com>
2019-12-22 23:01:54 +0100

    ssh key updated

--- src/components/info/Info.js
+++ src/components/info/Info.js
@@ -5,7 +5,7 @@ const Info = ({name, entries}) => {
 	return(
 		<div>
 		 <div className='white f3 center'>
-		  {`${name}, your current entry count ...`}
+		  {`${name}, your current entry count is...`}
 		 </div>
 		 <div className='white f1 center'>
 		  {entries}
